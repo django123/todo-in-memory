e7fd3191ed6b0a7530111475470d74dbfcf1ecd9
django123 <edougajean@gmail.com>
2021-10-28 14:32:09 +0100

    first commit

---
+++ src/main/java/com/enzo/todo/domain/ToDo.java
@@ -0,0 +1,37 @@
+package com.enzo.todo.domain;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class ToDo {
+
+    @NotNull
+    private String id;
+
+    @NotNull
+    @NotBlank
+    private String description;
+
+    private boolean completed;
+    private LocalDateTime created;
+    private LocalDateTime modified;
+
+
+    public ToDo() {
+        LocalDateTime date = LocalDateTime.now();
+
+        this.id = UUID.randomUUID().toString();
+        this.created = date;
+        this.modified = date;
+    }
+
+    public ToDo(String description) {
+        this();
+        this.description = description;
+    }
+}
---
+++ src/test/java/com/enzo/todo/TodoInMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzo.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoInMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
